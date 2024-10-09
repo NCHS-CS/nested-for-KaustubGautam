b262415fc30a8923e0d1f98211902b6ab97d37a8
KaustubGautam <2032486@apps.nsd.org>
2024-10-09 20:15:26 +0000

    done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,57 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+/*public class App{
+    //Practice 1
+    public static void main(String args[]){
+        int z=1;
+        for (int x=1; x<=5;x++){
+            for (int y=1; y<=z;y++){
+                System.out.print(x);
+            }
+            System.out.println();
+            z+=1;
 
-package org.nhs.app;
+        }
+        }
+    }*/
+    /*public class App{
+    for (int x=1; x<=5;x++){
+        for (int y=1; y<=4;y++){
+            System.out.print(x);
+        }
+        System.out.println();
+    }
+    }
+}*/
 
-// Your code goes here
+/*public class App{
+    //Practice 2
+    public static void main(String args[]){
+        int z=1;
+        for (int x=1; x<=5;x++){
+            for(int dot=5-z; dot>=1; dot--){
+                System.out.print(".");
+            
+            }
+            for (int y=1; y<=z;y++){
+                System.out.print(x);
+            }
+            z+=1;
+            
+          
+            System.out.println();
+        }
+        }
+    }*/
+//Practice problem three
+public class App{
+    public static void main(String args[]){
+        for(int line = 1; line <=5;line++){
+           for(int dot1=4-line; dot1>=0;dot1--)
+           System.out.print(".");    
+           System.out.print(line);
+        
+        for(int dot2=0; dot2<=line-1;dot2++)
+           System.out.print(".");
+           System.out.println();}
+     
+    }
+}
